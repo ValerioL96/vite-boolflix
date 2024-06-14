8ebc16dba7d208e638b2c24c7be6896c4db9d697
ValerioL96 <v.lembo1396@gmail.com>
2024-06-14 12:08:35 +0200

    Created Vue 3 project with Vite. Added sass and bootstrap.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
+@use './styles/partials/mixins' as *;
+@use './styles/partials/variables' as *;
+@use './styles/general';
+</style>
