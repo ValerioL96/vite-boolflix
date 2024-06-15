278ad191b7a77d5c3885c66f6c5cbbf3f4eb079b
ValerioL96 <v.lembo1396@gmail.com>
2024-06-15 16:44:48 +0200

    Created search bar in HeaderSearchBar component

---
+++ src/components/HeaderSearchBar.vue
@@ -0,0 +1,27 @@
+<script>
+
+import { store } from '../store.js';
+
+export default{
+  
+  data(){
+    return{
+      store,
+    }
+  }
+}
+
+</script>
+
+<template>
+
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Search for movies" aria-label="Search for movies" aria-describedby="button-addon2">
+  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+</div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/HeaderSearchElement.vue
@@ -0,0 +1,23 @@
+<script>
+
+import { store } from '../store.js';
+
+export default{
+  
+  data(){
+    return{
+      store,
+    }
+  }
+}
+
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
