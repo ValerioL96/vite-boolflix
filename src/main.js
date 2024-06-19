64890540f60b2bb31de93b722c7cf37e54fa2790
ValerioL96 <v.lembo1396@gmail.com>
2024-06-19 09:38:51 +0200

    Fixed general and main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from "/node_modules/.vite/deps/vue.js?v=03faadd8"
-import App from "/src/App.vue?t=1718749530238"
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount('#app');
+createApp(App).mount('#app')
